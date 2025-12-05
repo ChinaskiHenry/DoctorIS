cd43e24716de7617602b2c2eb3fe8c31588c503b
kornilovrp <kornilovrp@K418COMP11>
2025-12-05 12:24:22 +0300

    Добавьте файлы проекта.

---
+++ DoctorIS/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoctorIS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string ConnectionString = "Host=SQLstudv2;Port=5432;Database=23ipp11;Username=23ipp11;Password=23ipp11";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
